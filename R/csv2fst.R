123ace00706aef32e1e541c1e24e40504c7f0784
byandell <byandell@wisc.edu>
2025-02-23 15:35:18 -0600

    break out fst_rows as separate routine

--- R/csv2fst.R
+++ R/csv2fst.R
@@ -1,40 +1,50 @@
 #' Convert CSV to FST File
 #' 
-#' @param file_path path to CSV or FST file
+#' @param csv_path path to CSV or FST file
 #' 
 #' @importFrom data.table fread
-#' @importFrom dplyr filter group_by mutate n row_number select slice
-#' @importFrom tidyr pivot_wider
+#' @importFrom dplyr arrange
 #' @importFrom stringr str_detect str_replace
-#' @importFrom fst read_fst write_fst
+#' @importFrom fst write_fst
 #' @export
-csv2fst <- function(file_path) {
-  csv_name <- file_path
-  if(stringr::str_detect(csv_name, "csv$")) {
-    fst_name <- stringr::str_replace(csv_name, "csv$", "fst")
-    if(!file.exists(fst_name)) {
+csv2fst <- function(csv_path) {
+  if(stringr::str_detect(csv_path, "csv$")) {
+    fst_path <- stringr::str_replace(csv_path, "csv$", "fst")
+    if(!file.exists(fst_path)) {
       # Write FST file.
-      warning("Writing FST file: ", fst_name)
+      warning("Writing FST file: ", fst_path)
       fst::write_fst(
-        data.table::fread(csv_name, drop = "Which_mice") |> dplyr::arrange(Phenotype),
-        path = fst_name, compress = 100)
+        data.table::fread(csv_path, drop = "Which_mice") |> dplyr::arrange(Phenotype),
+        path = fst_path, compress = 100)
     }
   } else {
-    if(!stringr::str_detect(csv_name, "fst$"))
-      stop("No CSV or FST name provided: ", csv_name)
-    fst_name <- csv_name
+    if(!stringr::str_detect(csv_path, "fst$"))
+      stop("No CSV or FST name provided: ", csv_path)
+    fst_path <- csv_path
   }
-  row_name <- stringr::str_replace(fst_name, ".fst$", "_row.csv")
-  if(!file.exists(row_name)) {
+  return(fst_path)
+}
+#' Convert CSV to FST File
+#' 
+#' @param fst_path path to FST file
+#' 
+#' @importFrom dplyr filter group_by mutate n row_number select slice
+#' @importFrom tidyr pivot_wider
+#' @importFrom stringr str_replace
+#' @importFrom fst read_fst
+#' @export
+fst_rows <- function(fst_path) {
+  row_path <- stringr::str_replace(fst_path, ".fst$", "_row.csv")
+  if(!file.exists(row_path)) {
     # Database of first and last entries by phenotype
-    rows <- fst::read_fst(fst_name) |>
+    rows <- fst::read_fst(fst_path) |>
       dplyr::select(Phenotype) |>
       dplyr::mutate(rown = dplyr::row_number()) |>
       dplyr::group_by(Phenotype) |>
       dplyr::slice(c(1, dplyr::n())) |>
       dplyr::mutate(set = c("from", "to")) |>
       tidyr::pivot_wider(names_from = "set", values_from = "rown")
-    write.csv(rows, row_name, row.names = FALSE)
+    write.csv(rows, row_path, row.names = FALSE)
   }
-  return(c(row_name = row_name, fst_name = fst_name))
+  return(row_path)
 }
--- R/trait_scan.R
+++ R/trait_scan.R
@@ -25,15 +25,13 @@ trait_scan <- function(file_dir, selected_dataset, selected_trait) {
   
   scan_data <- tryCatch({
     # Convert CSV to FST (or retrieve FST name)
-    browser()
-    out <- csv2fst(file_dir$File_path[1])
-    row_name = out["row_name"]
-    fst_name = out["fst_name"]
+    fst_path <- csv2fst(file_dir$File_path[1])
+    row_path <- fst_rows(fst_path)
 
     # Read FST data.
     # Filter for rows where Phenotype matches selected_trait
-    rows <- dplyr::filter(read.csv(row_name), Phenotype == selected_trait)
-    data <- fst::read_fst(fst_name, from = rows$from, to = rows$to)
+    rows <- dplyr::filter(read.csv(row_path), Phenotype == selected_trait)
+    data <- fst::read_fst(fst_path, from = rows$from, to = rows$to)
     
     if (nrow(data) == 0) {
       stop("No data found for phenotype: ", selected_trait)
