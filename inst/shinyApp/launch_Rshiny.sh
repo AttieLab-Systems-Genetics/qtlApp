b44d5eb2724c08577fdd3a438d6a10293dc56843
byandell <byandell@wisc.edu>
2025-02-24 14:38:05 -0600

    add docker stuff

---
+++ inst/shinyApp/launch_Rshiny.sh
@@ -0,0 +1,9 @@
+
+docker stop mini-viewer-image_v2
+
+docker rm mini-viewer-image_v2
+
+docker build --build-arg LOCAL_DATA=/data/dev/miniViewer_3.0 -t mini-viewer-image_v2 .
+
+#RUN IT
+docker run -d -p 51175:3838 -v /data/dev/miniViewer_3.0:/data/dev/miniViewer_3.0 --name mini-viewer-image_v2 mini-viewer-image_v2
