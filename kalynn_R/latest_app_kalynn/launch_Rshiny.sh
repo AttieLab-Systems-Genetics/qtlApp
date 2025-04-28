a3fa878c0752a4cd7fc3f4acfe477d7c69ec7c87
Kalynn Willis <khwillis@ad.wisc.edu>
2025-04-28 10:10:29 -0500

    progress on app.R

--- kalynn_R/latest_app_kalynn/launch_Rshiny.sh
+++ kalynn_R/latest_app_kalynn/launch_Rshiny.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
 
 docker stop mini-viewer-image_v2
 
 docker rm mini-viewer-image_v2
 
-docker build --build-arg LOCAL_DATA=/data/dev/miniViewer_3.0 -t mini-viewer-image_v2 .
+# Add --no-cache to force rebuild including package installations
+docker build --no-cache --build-arg LOCAL_DATA=/data/dev/miniViewer_3.0 -t mini-viewer-image_v2 .
 
 #RUN IT
 docker run -m 30g -d -p 51175:3838 -v /data/dev/miniViewer_3.0:/data/dev/miniViewer_3.0 --name mini-viewer-image_v2 mini-viewer-image_v2
