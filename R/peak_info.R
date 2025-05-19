d8b674c934fb197aaebe6164a5cea9c3e869a736
Kalynn Willis <khwillis@ad.wisc.edu>
2025-05-19 11:19:21 -0500

    cleaning peak stuff

--- R/peak_info.R
+++ R/peak_info.R
@@ -1,25 +1,20 @@
 peak_info <- function(peak_table, scan_table,
   which_peak = NULL, click_table = NULL, xvar = "BPcum") {
   if (is.null(click_table)) {
-    # If no click data, try to get information from the selected peak instead
     if(is.null(which_peak)) return(NULL)
       return(peak_info_table(peak_table, which_peak))
   }
-  # If click data is available, process it with scan_table.
   if (is.null(scan_table)) return(NULL)
-  # Find nearest point if any.
   distances <- sqrt(
     (scan_table[[xvar]] - click_table$x)^2 + (scan_table$LOD - click_table$y)^2)
   nearest_point <- scan_table[which.min(distances), ]
   if (nrow(nearest_point) == 0) return(NULL)
-  # Return the peak closest to the clicked point.
   return(peak_info_table(peak_table, which_peak, nearest_point))
 }
 peak_info_table <- function(peak_table,
                             which_peak = nearest_point$markers,
                             nearest_point = NULL) {
   if(is.null(peak_table) || !nrow(peak_table)) return(NULL)
-  # Find peak by marker, considering possible column name differences
   peak_match <- NULL
   if ("marker" %in% colnames(peak_table) & !is.null(which_peak)) {
     peak_match <- dplyr::filter(peak_table, .data$marker == which_peak)
@@ -29,7 +24,6 @@ peak_info_table <- function(peak_table,
       trait = NA, cis = NA, ci_lo = NA, ci_hi = NA,
       A = NA, B = NA, C = NA, D = NA, E = NA, F = NA, G = NA, H = NA)
   }
-  # Create peak info table with safer column access
   myround <- function(x) {
     if(is.null(x) || is.na(x[1])) return(NA)
     if(is.numeric(x)) round(x[1], 3) else x[1]
@@ -44,7 +38,6 @@ peak_info_table <- function(peak_table,
       Cis        = myround(peak_match$cis),
       CI_Low     = myround(peak_match$ci_lo),
       CI_High    = myround(peak_match$ci_hi),
-      # Add strain effects if available
       AJ         = myround(peak_match$A),
       B6         = myround(peak_match$B),
       `129`      = myround(peak_match$C),
