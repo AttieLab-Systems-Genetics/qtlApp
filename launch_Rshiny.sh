f631e6509244168c4407b26dc04bf156bf276f23
Kalynn Willis <khwillis@ad.wisc.edu>
2025-08-10 21:36:58 -0500

    all done!

--- launch_Rshiny.sh
+++ launch_Rshiny.sh
@@ -3,11 +3,11 @@
 docker stop mini-viewer-image_v2
 docker rm mini-viewer-image_v2
 
-# Change to the root directory (where R/ lives)
-cd "$(dirname "$0")/../.." || exit 1
+# Change to the repository root (where this script and Dockerfile live)
+cd "$(dirname "$0")" || exit 1
 
 # Build using the root directory as context
-docker build --build-arg LOCAL_DATA=/data/dev/miniViewer_3.0 -t mini-viewer-image_v2 -f ./Dockerfile .
+docker build --build-arg LOCAL_DATA=/data/dev/miniViewer_3.0 -t mini-viewer-image_v2 -f Dockerfile .
 
 # Run the container
 docker run -m 30g -d -p 51175:3838 -v /data/dev/miniViewer_3.0:/data/dev/miniViewer_3.0 --name mini-viewer-image_v2 mini-viewer-image_v2
