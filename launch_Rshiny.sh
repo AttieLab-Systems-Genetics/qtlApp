55abf9fdb814caad797b8ed4c8a805bc60579739
Kalynn Willis <khwillis@ad.wisc.edu>
2025-08-10 21:22:58 -0500

    refactor(fs): reorganize R/ into modules, plots, data, ui, utils; move data_handling.R into R/data

--- launch_Rshiny.sh
+++ launch_Rshiny.sh
@@ -7,7 +7,7 @@ docker rm mini-viewer-image_v2
 cd "$(dirname "$0")/../.." || exit 1
 
 # Build using the root directory as context
-docker build --build-arg LOCAL_DATA=/data/dev/miniViewer_3.0 -t mini-viewer-image_v2 -f kalynn_R/latest_app_kalynn/Dockerfile .
+docker build --build-arg LOCAL_DATA=/data/dev/miniViewer_3.0 -t mini-viewer-image_v2 -f ./Dockerfile .
 
 # Run the container
 docker run -m 30g -d -p 51175:3838 -v /data/dev/miniViewer_3.0:/data/dev/miniViewer_3.0 --name mini-viewer-image_v2 mini-viewer-image_v2
